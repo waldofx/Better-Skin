8c8d1fbd0b9ae24cab4b4014e0d9628baf0a16bf
waldofx <90760767+waldofx@users.noreply.github.com>
2022-11-17 15:13:15 +0700

    Homepage - lepin

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Footer() {
-    return (
-        <div>
-            <p>© 2022 Better Skin</p>
-        </div>
-    );
+  return (
+    <footer class="py-3 bg-dark text-white-50">
+      <div class="container text-center">
+        <small>Copyright &copy; 2022 Better Skin</small>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
