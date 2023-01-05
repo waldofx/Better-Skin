6cc8b7b113c1410ca42c7bdb6c93f4288c77bb57
Rexoll <108333831+Rexoll@users.noreply.github.com>
2023-01-05 20:23:43 +0700

    daftar produk by alifa

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import "./home.css";
 
+// import hooks
+import useGetProductsByName from "../hooks/useGetProductsByName";
+import useInsertProducts from "../hooks/useInsertProducts";
+import { from } from "@apollo/client";
+
 function Home() {
+    //get products
+    // ----------------- custom hook graphql -------------------------
+    const { dataByName, loadingDataByName, errorDataByName } = useGetProductsByName();
+
+    //setproducts at the start of render using useeffect
+    const [productdatas, setProducts] = useState([]);
+    useEffect(() => {
+        if (dataByName) {
+            setProducts(dataByName.products);
+        }
+    }, [dataByName]);
+
+    //error + loading
+    const isError = errorDataByName;
+    const isLoading = loadingDataByName;
+
+    console.log("Product data:", productdatas);
     return (
         <div>
             <Header />
             <div class="container-fluid">
                 <section id="section1">
-                    
+
                     <img src="bg1.png" class="rounded img-fluid img-thumbnail mt-5"></img>
-                    
+
                     <h1 class="mt-5">Best Seller</h1>
                     <p>Created for every skin type, need, and concern to get the glowy skin you've been dreaming of.</p>
                     <p>Better skin is a new approach to beauty. It's about fun and freedom and being OK with yourself today.</p>
@@ -23,14 +45,14 @@ function Home() {
 
                         <img src="bg10.png" class="w-5 shadow-1-strong rounded img-fluid img-thumbnail" alt="bg10" />
                     </div>
-                    
+
                 </section>
                 <section id="section2">
 
                 </section>
                 <section id="section3">
                     <p id="new">NEW FEATURED PRODUCT</p>
-                    <div class="row row-cols-1 row-cols-md-4 g-4">
+                    <div class="row row-cols-1 row-cols-md-4 g-4 mb-5">
                         <div class="col">
                             <div class="card h-100">
                                 <img src="bg6.png" class="card-img-top" alt="bg6" />
@@ -83,6 +105,39 @@ function Home() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div>
+                        {/* INI ADALAH LIST PRODUK */}
+                        <div className="container">
+                            <h1 className="mb-5">Daftar Product</h1>
+                            {isError && <p>Something Went Wrong...</p>}
+                            {isLoading && <p>Now loading...</p>}
+                            {!isError && !isLoading && (
+                                <div>
+                                    <div className="container">
+                                        <div className="row">
+                                            {productdatas.map((productdata) => (
+                                                <div className="col-md-3 mb-2">
+                                                    <div class="card">
+                                                        <img src={productdata.img} class="card-img-top" alt="..." />
+                                                        <div class="card-body">
+                                                            <div className="row">
+                                                                <div className="d-flex justify-content-between align-items-center mt-2 mb-2">
+                                                                    <h5 class="card-text fw-bold">{productdata.name}</h5>
+                                                                    <p class="card-text">{productdata.price}</p>
+                                                                </div>
+                                                                <a className="btn btn-danger" href="#">Add To Cart</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                        {/* INI ADALAH AKHIR LIST PRODUK */}
                     </div>
                 </section>
             </div>
