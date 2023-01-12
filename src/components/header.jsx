6f0a203e5578f2fbd6125d6e446cfc0b876461e1
waldofx <90760767+waldofx@users.noreply.github.com>
2023-01-12 15:59:03 +0700

    auto formatting + header update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,93 +1,73 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 //import hooks
 import { addAdminData } from "../store/adminDataSlice";
 
 function Header() {
-  const [isActive, setActive] = useState(false);
+    //auth check
+    let adminData = useSelector((state) => state.adminData.adminData);
+    let isAdmin = adminData.status;
+    const dispatch = useDispatch();
 
-  function toggleHidden() {
-    setActive(!isActive);
-  }
+    function handleLogout() {
+        adminData = {
+            img: "",
+            name: "",
+            status: false,
+        };
+        dispatch(addAdminData(adminData));
+        alert("Berhasil logout!");
+        window.location.reload(false);
+    }
 
-  //auth check
-  let adminData = useSelector((state) => state.adminData.adminData);
-  let isAdmin = adminData.status;
-  const dispatch = useDispatch();
-
-  function handleLogout() {
-    adminData = {
-      img: "",
-      name: "",
-      status: false,
-    };
-    dispatch(addAdminData(adminData));
-    alert("Berhasil logout!");
-    window.location.reload(false);
-  }
-
-  return (
-    <div className="header">
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            Better Skin
-          </a>
-          <div class=" collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ms-auto ">
-              <li class="nav-item active">
-                <a class="nav-link" href="/product">
-                  Product
-                </a>
-              </li>
-              {isAdmin && (
-                <li class="nav-item active">
-                  <a href="/transactionList" class="nav-link">
-                    Transactions
-                  </a>
-                </li>
-                    )}
-            </ul>
-            <ul class="navbar-nav ms-auto d-none d-lg-inline-flex">
-              {!isAdmin ? (
-                <li class="nav-item active">
-                  <a class="nav-link" href="/login">
-                    Login
-                  </a>
-                </li>
-              ) : (
-                <ul class="navbar-nav ms-auto ">
-                <li class="nav-item active">
-                  <a
-                    class="nav-link"
-                    href="/adminDashboard"
-                  >
-                    Dashboard
-                  </a>
-              </li>
-                <li class="nav-item active">
-                  <a
-                    class="nav-link"
-                    onClick={handleLogout}
-                    style={{ cursor: "pointer" }}
-                  >
-                    Logout
-                  </a>
-                </li>
-                </ul>
-              )}
-              <li class="nav-item">
-                <a class="nav-link" href="/userDashboard">
-                  Cart
-                </a>
-              </li>
-            </ul>
-          </div>
+    return (
+        <div className="header">
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/">
+                        Better Skin
+                    </a>
+                    <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav ms-auto ">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="/product">
+                                    Product
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ms-auto d-none d-lg-inline-flex">
+                            {!isAdmin ? (
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="/login">
+                                        Login
+                                    </a>
+                                </li>
+                            ) : (
+                                <ul class="navbar-nav ms-auto ">
+                                    {/* <li class="nav-item active">
+                                        <a class="nav-link" href="/adminDashboard">
+                                            Dashboard
+                                        </a>
+                                    </li> */}
+                                    <li class="nav-item active">
+                                        <a class="nav-link" onClick={handleLogout} style={{ cursor: "pointer" }}>
+                                            Logout
+                                        </a>
+                                    </li>
+                                </ul>
+                            )}
+                            <li class="nav-item">
+                                <a class="nav-link" href="/userDashboard">
+                                    Cart
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
-      </nav>
-    </div>
-  );
+    );
 }
 
 export default Header;
--- src/pages/DetailProduct/detailproduct.jsx
+++ src/pages/DetailProduct/detailproduct.jsx
@@ -10,72 +10,68 @@ import "./detailproduct.css";
 import "../../hooks/useHoover.js";
 
 const GetProductByID = gql`
-  query MyQuery($id: Int!) {
-    products(where: { id: { _eq: $id } }) {
-      id
-      name
-      price
-      initial_stock
-      final_stock
-      total
-      img
-      desc
-      desc2
-      desc3
+    query MyQuery($id: Int!) {
+        products(where: { id: { _eq: $id } }) {
+            id
+            name
+            price
+            initial_stock
+            final_stock
+            total
+            img
+            desc
+            desc2
+            desc3
+        }
     }
-  }
 `;
 
 function DetailProduct() {
-  //initial detail
-  const { id } = useParams();
-  const { data } = useQuery(GetProductByID, {
-    variables: { id },
-  });
+    //initial detail
+    const { id } = useParams();
+    const { data } = useQuery(GetProductByID, {
+        variables: { id },
+    });
 
-  //setproducts at the start of render using useeffect
-  const [productdata, setProducts] = useState({});
-  useEffect(() => {
-    if (data) {
-      setProducts(data.products[0]);
-    }
-  }, [data]);
+    //setproducts at the start of render using useeffect
+    const [productdata, setProducts] = useState({});
+    useEffect(() => {
+        if (data) {
+            setProducts(data.products[0]);
+        }
+    }, [data]);
 
-  console.log("datas", data);
-  console.log("product data", productdata);
+    console.log("datas", data);
+    console.log("product data", productdata);
 
-  return (
-    <div>
-      <Header />
-      <div className="product">
-        <div className="container">
-          <div className="product-div">
-            <div className="product-left">
-              <div className="img-product-left">
-                <img
-                  src={productdata.img}
-                  alt="img-product"
-                  style={{ width: "300px", height: "500px" }}
-                />
-              </div>
+    return (
+        <div>
+            <Header />
+            <div className="product">
+                <div className="container">
+                    <div className="product-div">
+                        <div className="product-left">
+                            <div className="img-product-left">
+                                <img src={productdata.img} alt="img-product" style={{ width: "300px", height: "500px" }} />
+                            </div>
+                        </div>
+
+                        <div className="product-right">
+                            <span className="product-name"> {productdata.name} </span>
+                            <span className="product-price"> Rp {productdata.price}</span>
+                            <p className="product-description">{productdata.desc}</p>
+                            <div className="btn-groups">
+                                <button type="button" className="fas add-cart-btn">
+                                    <i className="txt-btn"></i> Buy now
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="product-right">
-              <span className="product-name"> {productdata.name} </span>
-              <span className="product-price"> Rp {productdata.price}</span>
-              <p className="product-description">{productdata.desc}</p>
-              <div className="btn-groups">
-                <button type="button" className="fas add-cart-btn">
-                  <i className="txt-btn"></i> Buy now
-                </button>
-              </div>
-            </div>
-          </div>
+            <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-  );
+    );
 }
 
 export default DetailProduct;
